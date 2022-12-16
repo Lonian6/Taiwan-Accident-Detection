26ec876c617b64f4dea7379cb0dcd184cec209e1
Lonian6 <87214157+Lonian6@users.noreply.github.com>
2022-12-16 19:58:47 +0800

    Update install.sh
    make sure all of the download videos are .mp4 files

--- crawler/install.sh
+++ crawler/install.sh
@@ -4,4 +4,4 @@ cat $1 | awk -F "," '{
     if(NR!=1){
         printf("%s \n", $5)
     }
-}' | xargs yt-dlp -N 10 -P $2
+}' | xargs yt-dlp --format "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]" -N 10 -P $2
